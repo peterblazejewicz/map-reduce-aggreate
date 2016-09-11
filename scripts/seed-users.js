9d42c6df96085634d561e935ebcd7828bffe51b3
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-09-11 15:09:30 +0200

    Convert dates on-the-fly to ISODate

--- scripts/seed-users.js
+++ scripts/seed-users.js
@@ -15,6 +15,10 @@ print('removing data');
 var deleted = db.users.drop();
 // bulk insert users
 print('bulk insert data');
+users.forEach(function(user) {
+    user.dob = ISODate(user.dob.replace(' ', 'T'));
+    user.registered = ISODate(user.registered.replace(' ', 'T'));
+});
 var results = db.users.insert(users);
 printjson(results);
 // quit
