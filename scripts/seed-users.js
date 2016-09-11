f3fa1019fe54bdec14f30d2b93e9d692b94deb08
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-09-11 20:23:27 +0200

    Remove data transformation code from seed script
    The data transformation should be done
    alone in mapper and aggregation

--- scripts/seed-users.js
+++ scripts/seed-users.js
@@ -15,10 +15,6 @@ print('removing data');
 var deleted = db.users.drop();
 // bulk insert users
 print('bulk insert data');
-users.forEach(function(user) {
-    user.dob = ISODate(user.dob.replace(' ', 'T'));
-    user.registered = ISODate(user.registered.replace(' ', 'T'));
-});
 var results = db.users.insert(users);
 printjson(results);
 // quit
