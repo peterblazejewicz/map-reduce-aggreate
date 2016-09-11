f45acd2bc883a1564f6c48c02f15f3cc648f9f52
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-09-11 20:24:08 +0200

    Map-reduce and aggregation in single example

--- scripts/map-reduce-user-transforms.js
+++ scripts/map-reduce-user-transforms.js
@@ -1,15 +1,57 @@
 'use strict';
 // use #test
-var client = new Mongo();
+const client = new Mongo();
 var db = client.getDB('test');
-// util(s)
-var toUpperCase = function(txt) {
-    if(!txt) return txt;
-    return txt.toUpperCase();
+//
+const FUNCTIONS = [];
+// cleanup
+const cleanup = () => {
+    FUNCTIONS.forEach((doc) =>  {
+        db.system.js.remove(doc);
+    });
 };
-db.system.js.save({
-    _id: 'toUpperCase',
-    value: toUpperCase
+// compute quater
+var computeQuarterForISODate = (date) => {
+    var month = date.getMonth() + 1;
+    if (month > 9) return 4;
+    if (month > 6) return 3;
+    if (month > 3) return 2;
+    return 1;
+};
+FUNCTIONS.push({
+    _id: 'computeQuarterForISODate',
+    value: computeQuarterForISODate
+});
+// compute country (nat) category
+var computeCategory = (nat) => {
+    if(!nat) return null;
+    switch(nat.toLowerCase()) {
+        case 'dk':
+        case 'fi':
+        case 'nl':
+        case 'pl':
+            return 'EU';
+        case 'BR':
+            return 'Other';
+        default: {
+            return "Other";
+        }
+    }
+};
+FUNCTIONS.push({
+    _id: 'computeCategory',
+    value: computeCategory
+});
+var convertToISODate = (str) => {
+    return ISODate(str.replace(' ', 'T'));
+};
+FUNCTIONS.push({
+    _id: 'convertToISODate',
+    value: convertToISODate
+})
+//
+FUNCTIONS.forEach((doc) => {
+    db.system.js.save(doc);
 });
 // mapper
 var mapper = function() {
@@ -17,19 +59,21 @@ var mapper = function() {
     var value = {
         "gender": this.gender,
         "name": {
-            "title" : toUpperCase(this.name.title),
-		    "first" : toUpperCase(this.name.first),
-		    "last" : toUpperCase(this.name.last)
+            "title": this.name.title,
+            "first": this.name.first,
+            "last": this.name.last,
+            "fullName": `${this.name.first} ${this.name.last}`
         },
         "location": {
-            "street" : this.location.street,
-            "city" : this.location.city,
-            "state" : this.location.state,
-            "postcode" : this.location.postcode
+            "street": this.location.street,
+            "city": this.location.city,
+            "state": this.location.state,
+            "postcode": this.location.postcode
         },
         "email": this.email,
-        "dob": ISODate(this.dob),
-        "registered": ISODate(this.dob),
+        "dob": convertToISODate(this.dob),
+        "quarter": computeQuarterForISODate(convertToISODate(this.dob)),
+        "registered": convertToISODate(this.registered),
         "phone": this.phone,
         "cell": this.cell,
         "id": {
@@ -41,30 +85,66 @@ var mapper = function() {
             "medium": this.picture.medium,
             "thumbnail": this.picture.thumbnail
         },
-        "nat": this.nat
+        "nat": this.nat,
+        "category": computeCategory(this.nat)
     };
     emit(key, value);
 };
-var reducer = function(key, values) {
-    return key;
+const reducer = (key, values) => {
+    // this won't be called
+    return values[0];
 };
-var finalizer = function(key, reducedValues) {
-    return reducedValues;
-}
-// cleanup
-db.user_transformed.drop();
 // reduce
-db.users.mapReduce(
+var results = db.users.mapReduce(
     mapper,
     reducer,
     {
-        out: "reduced_users",
-        finalize: finalizer
+        out: 'users_mapped'
     }
 );
-// remove helpers
-db.system.js.remove({
-    _id: 'toUpperCase'
-});
-// Quit
+if(results.ok !== 1) {
+    print('failed to map users');
+    cleanup();
+    quit();
+}
+//
+results = db.users_mapped.aggregate([
+    {
+        $project: {
+            _id: 1,
+            gender: '$value.gender',
+            first: '$value.name.first',
+            last: '$value.name.last',
+            fullName: '$value.name.fullName',
+            "email": '$value.email',
+            "dob": '$value.dob',
+            "year": {
+                $year: '$value.dob'
+            },
+            "quarter": '$value.quarter',
+            "month": {
+                $month: '$value.dob'
+            },
+            "week": {
+                $week: '$value.dob'
+            },
+            "registered": '$value.registered',
+            "phone": '$value.phone',
+            "cell": '$value.cell',
+            "picture": {
+                "large": '$value.picture.large',
+                "medium": '$value.picture.medium',
+                "thumbnail": '$value.picture.thumbnail'
+            },
+            "nat": '$value.nat',
+            "category": '$value.category'
+        }
+    },
+    {
+        $out: 'users_mapped'
+    }
+]);
+printjson(results);
+// quit
+cleanup();
 quit();
